5e275448797e91f1b524b8e8edaa19bb3aacc804
victornaziozeno <99661689+victornaziozeno@users.noreply.github.com>
2022-02-17 10:45:36 -0300

    Update Media, nota maior igual 9.5.cpp

--- Algoritmos/Media, nota maior igual 9.5.cpp
+++ Algoritmos/Media, nota maior igual 9.5.cpp
@@ -1,14 +1,17 @@
 #include <stdio.h>
-
+#define NT 9.5
+#define FQ 75
 int main(){
-float notaA,notaB;
+float notaA,notaB,freq;
     
     printf("Digite a nota A: ");
     scanf("%f",&notaA);
     printf("Digite a nota B: ");
-    scanf("%f",&notaB); 
+    scanf("%f",&notaB);
+    printf("Digite a frequencia: ");
+    scanf("%f",&freq);
    
-   if ((notaA+notaB)/2>=9.5)
+   if ((notaA+notaB)/2>=NT&&freq>=FQ)
     printf("Aluno aprovado!");
     else printf("Aluno reprovado!");
 }
