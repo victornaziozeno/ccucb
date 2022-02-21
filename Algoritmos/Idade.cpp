be663566022f1e77b6d3373d15992410bd300da0
victornaziozeno <99661689+victornaziozeno@users.noreply.github.com>
2022-02-21 09:47:38 -0300

    Update Idade.cpp

--- Algoritmos/Idade.cpp
+++ Algoritmos/Idade.cpp
@@ -9,13 +9,13 @@ int main(){
     
     if(idade>=0&&idade<=10)
     printf("Crianca");
-    else if(idade>10&&idade<18)
+    else if(idade<18)
     printf("Adolescente");
-    else if(idade>=18&&idade<60)
+    else if(idade<60)
     printf("Adulto");
-    else if(idade>=60&&idade<100)
+    else if(idade<100)
     printf("Idoso");
-    else if(idade>=100&&idade<150)
+    else if(idade<150)
     printf("Zumbi");
     else printf("Idade invalida");
 
