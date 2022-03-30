d3fc0f412ea96985556cf39bdb881de091c04af3
victornaziozeno <99661689+victornaziozeno@users.noreply.github.com>
2022-03-30 10:12:43 -0300

    Add files via upload

---
+++ Algoritmos/Propostos/Ifelsebloco.cpp
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int main(void){
+	int idade=1;
+while (1){
+	printf("ELEITORAL\n\n");	
+	printf("Digite sua idade: \n\n");	
+	scanf("%d",&idade);
+	
+	if(idade<=0){
+	printf("Idade Invalida\n\n");
+	}else{
+	if(idade<16){
+	printf("Nao pode votar\n\n");
+	}else{
+	if(idade<18){
+	printf("Opcional\n\n");
+	}else{
+	if (idade<65){
+	printf("Obrigatorio\n\n");
+	}else{
+	if(idade==65){
+	printf("Premio 1\n");
+	}else{
+	if (idade<82);{
+	printf("Obrigatorio\n");}
+	if(idade==82){
+	printf("Premio 2\n");
+	}else{
+	if (idade>82);{
+	printf("Opcional\n");}
+return 0;
+}}}}}}}}
